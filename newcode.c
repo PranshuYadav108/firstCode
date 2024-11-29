f581c08961128389b4881656d12cdf1cbe7c85a9
PranshuYadav108 <pranshu16082004@gmail.com>
2024-11-29 13:18:11 +0530

    add new file

---
+++ newcode.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+
+
+    printf("(a + b)^2 = %d\n is ", a*b);
+
+    return 0;
+}
